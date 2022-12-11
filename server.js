6299f012d9caf7d6e6b82ef3d7571e35bc4c8714
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2022-12-10 22:01:53 -0500

    Added the comments to the server script

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Imports
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -12,14 +13,17 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+// Sets the homepage to the index file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+// Gets for the notes from the database
 app.get('/api/notes', (req, res) => {
   res.json(notes);
 });
 
+// Posts a new note to the database
 app.post('/api/notes', (req, res) => {
   const { title, text } = req.body;
   
@@ -32,15 +36,18 @@ app.post('/api/notes', (req, res) => {
   
   const notesStr = JSON.stringify(notes, null, '\t');
 
+  // Adds the updated list back to the database
   fs.writeFile(file, notesStr, (err) => {
     err ? console.error(err) : res.json(`Note ${noteObj.id} was added to the database`);
   });
 });
 
+// Deletes a note based on their id from the database
 app.delete('/api/notes/:id', (req, res) => {
   const noteId = req.params.id;
   const noOfNotes = notes.length;
   
+  // Cycles through the notes to find the note to delete
   for (let i = 0; i < notes.length; i++) {
     const note = notes[i];
     
@@ -50,21 +57,25 @@ app.delete('/api/notes/:id', (req, res) => {
     }
   }
 
+  // If no ID matches the one provided, it lets the user know 
   if (noOfNotes === notes.length) {
     res.json(`There is no note with an id of ${ noteId }`)
   }
   
   const notesStr = JSON.stringify(notes, null, '\t');
 
+  // Adds the updated list back to the database
   fs.writeFile(file, notesStr, (err) => {
     err ? console.error(err) : res.json(`${noteId} was removed from the database`);
   });
 });
 
+// Routes all dead endpoints to the homepage
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+// Posts a message upon server load
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
