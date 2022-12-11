a6debc0721c547865b6bf1ef0382ae0ea5337397
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2022-12-10 21:38:56 -0500

    Added logic for the POST API call

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const notes = require('./db/db.json')
 
 const PORT = 3001;
 const app = express();
+const file = './db/db.json';
 
 app.use(express.static('public'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/public/index.html'));
@@ -17,7 +21,20 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-  // 
+  const { title, text } = req.body;
+  
+  const noteObj = {
+    id: Math.floor((Math.random() + 1) * 0x10000).toString(16).substring(1),
+    title,
+    text
+  };
+  notes.push(noteObj);
+  
+  const notesStr = JSON.stringify(notes, null, '\t');
+
+  fs.writeFile(file, notesStr, (err) => {
+    err ? console.error(err) : res.json(`Note ${noteObj.id} was added to the database`);
+  });
 });
 
 app.delete('/api/notes/:id', (req, res) => {
